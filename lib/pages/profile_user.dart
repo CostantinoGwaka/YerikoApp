bcd88a0f51837fbace8b6006f3aff8769c09a87a
Costantino Dionis Gwaka <gwaka94@gmail.com>
2025-07-08 20:11:49 +0300

    fix

--- lib/pages/profile_user.dart
+++ lib/pages/profile_user.dart
@@ -102,8 +102,6 @@ class _ProfilePageState extends State<ProfilePage> {
           //end here
           // ignore: use_build_context_synchronously
           Navigator.pop(rootContext);
-          // ignore: use_build_context_synchronously
-          Navigator.pop(rootContext);
 
           // ignore: use_build_context_synchronously
           ScaffoldMessenger.of(rootContext).showSnackBar(

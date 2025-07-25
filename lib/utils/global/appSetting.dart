0547fe8e3e495fcc2e9ee46777c63e87ba52849f
Costantino Dionis Gwaka <gwaka94@gmail.com>
2025-07-25 12:41:02 +0300

    fix show active jumuiya

--- lib/utils/global/appSetting.dart
+++ lib/utils/global/appSetting.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: file_names
 
 class AppSettings {
-  static const appVersion = "1.0.0 (11)";
-  static const oldversionCode = "10";
-  static const newversionCode = "11";
+  static const appVersion = "1.0.0 (12)";
+  static const oldversionCode = "11";
+  static const newversionCode = "12";
 }

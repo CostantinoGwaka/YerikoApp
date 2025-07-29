52d316510279a6b4b6436e1f3b4cf3ac550967d4
Costantino Dionis Gwaka <gwaka94@gmail.com>
2025-07-29 21:13:26 +0300

    fix message

--- lib/utils/global/appSetting.dart
+++ lib/utils/global/appSetting.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: file_names
 
 class AppSettings {
-  static const appVersion = "1.0.0 (13)";
-  static const oldversionCode = "12";
-  static const newversionCode = "13";
+  static const appVersion = "1.0.0 (14)";
+  static const oldversionCode = "13";
+  static const newversionCode = "14";
 }

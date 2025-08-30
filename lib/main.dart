13eed9529bf714f20ffe3d48db6f3ad5b16238ae
Costantino Dionis Gwaka <gwaka94@gmail.com>
2025-08-30 21:49:43 +0300

    minot

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localization/flutter_localization.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:jumuiya_yangu/models/auth_model.dart';
@@ -12,12 +13,14 @@ import 'package:jumuiya_yangu/pages/home_page.dart';
 import 'package:jumuiya_yangu/pages/login_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:jumuiya_yangu/shared/localstorage/index.dart';
+import 'package:jumuiya_yangu/theme/colors.dart';
 import 'package:jumuiya_yangu/utils/url.dart';
 import 'package:http/http.dart' as http;
 
 LoginResponse? userData;
 ActiveChurchYearResponse? currentYear;
-final GlobalKey<NavigatorState> globalNavigatorKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> globalNavigatorKey =
+    GlobalKey<NavigatorState>();
 
 Future<void> getCurrentChurchYearData() async {
   try {
@@ -42,6 +45,18 @@ Future<void> getCurrentChurchYearData() async {
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterLocalization.instance.ensureInitialized();
+
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: mainFontColor, // 🔹 Status bar color
+    statusBarIconBrightness: Brightness.light, // 🔹 Icon/text color
+  ));
+
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarBrightness:
+        Brightness.light, // 🔹 For iOS (controls background behind status bar)
+    statusBarIconBrightness:
+        Brightness.light, // 🔹 For Android (controls status bar icons)
+  ));
   await getCurrentChurchYearData();
   runApp(Phoenix(child: const MyApp()));
 }
@@ -81,6 +96,10 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: GoogleFonts.poppins().fontFamily,
+        appBarTheme: AppBarTheme(
+          backgroundColor: mainFontColor,
+          foregroundColor: mainFontColor,
+        ),
       ),
       supportedLocales: const [
         Locale('en', 'US'),
@@ -91,7 +110,9 @@ class _MyAppState extends State<MyApp> {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      home: (userData != null && userData!.user.phone!.isNotEmpty) ? HomePage() : LoginPage(),
+      home: (userData != null && userData!.user.phone!.isNotEmpty)
+          ? HomePage()
+          : LoginPage(),
     );
   }
 }

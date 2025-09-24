6f081dde335d3f4fed78d52914ace30dcc31d0c8
Costantino Dionis Gwaka <gwaka94@gmail.com>
2025-09-24 21:12:15 +0300

    add report module

--- lib/utils/global/appSetting.dart
+++ lib/utils/global/appSetting.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: file_names
 
 class AppSettings {
-  static const appVersion = "1.0.0 (16)";
-  static const oldversionCode = "15";
-  static const newversionCode = "16";
+  static const appVersion = "1.0.0 (17)";
+  static const oldversionCode = "16";
+  static const newversionCode = "17";
 }

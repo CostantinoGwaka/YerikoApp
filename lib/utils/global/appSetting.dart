616eac1152008a67fb1568196820299cca6ef032
Costantino Dionis Gwaka <gwaka94@gmail.com>
2025-11-27 13:20:21 +0300

    add new feature update password

--- lib/utils/global/appSetting.dart
+++ lib/utils/global/appSetting.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: file_names
 
 class AppSettings {
-  static const appVersion = "1.0.0 (17)";
-  static const oldversionCode = "16";
-  static const newversionCode = "17";
+  static const appVersion = "1.0.0 (18)";
+  static const oldversionCode = "17";
+  static const newversionCode = "18";
 }

6a3f37aa0515e57c1a01bad4a771b65a751e7cdb
Costantino Dionis Gwaka <gwaka94@gmail.com>
2025-11-28 10:04:44 +0300

    fix: app version and ndk

--- lib/utils/global/appSetting.dart
+++ lib/utils/global/appSetting.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: file_names
 
 class AppSettings {
-  static const appVersion = "1.0.0 (19)";
+  static const appVersion = "1.0.0 (20)";
   static const oldversionCode = "17";
-  static const newversionCode = "19";
+  static const newversionCode = "20";
 }

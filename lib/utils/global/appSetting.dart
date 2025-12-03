164acaf57427a36f4222037164ac2b5b317a7b3d
Costantino Dionis Gwaka <gwaka94@gmail.com>
2025-12-03 08:26:52 +0300

    fix: loan apps users

--- lib/utils/global/appSetting.dart
+++ lib/utils/global/appSetting.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: file_names
 
 class AppSettings {
-  static const appVersion = "1.0.0 (25)";
-  static const oldversionCode = "24";
-  static const newversionCode = "25";
+  static const appVersion = "1.0.0 (27)";
+  static const oldversionCode = "26";
+  static const newversionCode = "27";
 }

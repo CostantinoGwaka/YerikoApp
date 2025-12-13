0d0814c81b3d3604a9493fb9387b826f08261f63
Costantino Dionis Gwaka <gwaka94@gmail.com>
2025-12-13 09:35:44 +0300

    upgrade app

--- lib/utils/global/appSetting.dart
+++ lib/utils/global/appSetting.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: file_names
 
 class AppSettings {
-  static const appVersion = "1.0.0 (27)";
-  static const oldversionCode = "26";
-  static const newversionCode = "27";
+  static const appVersion = "1.0.0 (28)";
+  static const oldversionCode = "27";
+  static const newversionCode = "28";
 }
